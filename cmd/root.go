1d1f1506e7c283fdb2ad236cb3836dc1ae87218e
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2024-01-19 17:45:59 -0800

    Add error handling for if/when print fails

--- cmd/root.go
+++ cmd/root.go
@@ -16,7 +16,9 @@ var rootCmd = &cobra.Command{
 
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
-		fmt.Fprintf(os.Stderr, "Whoops. There was an error while executing your CLI '%s'", err)
-		os.Exit(1)
+		_, err := fmt.Fprintf(os.Stderr, "Whoops. There was an error while executing your CLI '%s'", err)
+		if err != nil {
+			os.Exit(1)
+		}
 	}
 }
